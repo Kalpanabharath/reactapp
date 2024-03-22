d7c01f0d628dc91211a5dc99b869718ed2286469
Kalpanabharath <stonepanakeerthu@gmail.com>
2024-03-22 21:16:45 +0530

    conditional component using ternary oprator

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
-  state={isloggin:true}
+  state={isloggin:false}
 render(){
     let {isloggin}=this.state
-    let displaybtn;
-    if (isloggin===true){
-      displaybtn = <button  type='button'>LOG OUT</button>
-    }else{
-      displaybtn = <button  type='button'>LOG IN</button>
-    }
+    
     return (
       <div className="container">
         <h1>Hello, User</h1>
-         {displaybtn}
+         {isloggin?<button>LOG OUT</button>:<button>LOG IN</button>}
       </div>
     
     )
