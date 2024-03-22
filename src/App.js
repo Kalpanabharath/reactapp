6560a682cad56983059d36952ee5605998434e28
Kalpanabharath <stonepanakeerthu@gmail.com>
2024-03-22 20:57:59 +0530

    conditional component user login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import Mycomponent from './components/head';
+
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
+  state={isloggin:false}
+
+
+  buttonDisplay=()=>{
+    let {isloggin}=this.state
+    if (isloggin===true){
+      return <button  type='button'>LOG OUT</button>
+    }
+    return <button  type='button'>LOG IN</button>
+  }
   render(){
-    return <Mycomponent  />
+    return (
+      <div className="container">
+        <h1>Hello, User</h1>
+        {this.buttonDisplay()}
+      </div>
+    
+    )
   }
 }
 export default App;
