9973ba06c0d81df90f1ff86ea666b0dc43b5b306
Kalpanabharath <stonepanakeerthu@gmail.com>
2024-03-22 14:30:40 +0530

    state practice for speed project

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -3,21 +3,36 @@ import './index.css';
 
 
 class Mycomponent extends Component{
-    state={counting:0}
-    
-   onIncrement = () =>{
-    this.setState((prevState)=>({counting:prevState.counting+1}))
-   }
-   render() {
-    let {counting}=this.state
+    state = { speed:0 }
+    increment=()=>{
+        this.setState((previous)=>{
+            let retspeed=previous.speed== 240 ?{speed:200}:{speed:previous.speed + 10}
+            return retspeed
+        })
+
+        
+    }
+    decrement=()=>{
+        this.setState((previous)=>{
+            let speed=previous.speed==0?{speed:0}:{speed:previous.speed - 10}
+           return speed })
+    }
+
+render(){
+    let { speed }=this.state;
     return (
         <div className="container">
-            <h3 >Tha buttion has been clicked {counting} times</h3>
-            <p>click tha button to increase tha count</p>
-            <button onClick={this. onIncrement}>Clickme</button>
+        <img src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"/>
+        <h2>speed is {speed} mph</h2>
+        <p>minimum limit is 0 mph & max limit is 200 mph </p>
+        <div>
+            <button onClick={this.increment}>Accelerate </button>
+            <button onClick={this.decrement}> Brake</button>
         </div>
-    )
-   }
+        </div>
+        
 
- }
-export default Mycomponent
+    )
+}
+}
+export default Mycomponent;
